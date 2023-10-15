38ea071084a3817d7b244bdf86577a459da27556
untill265 <147309886+untill265@users.noreply.github.com>
2023-10-15 19:10:44 +0500

    Initial commit

---
+++ lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
